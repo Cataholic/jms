fcf3e0944bf00e3514d8ef7910c518f05d679aa1
Cataholic <965555511@qq.com>
2021-04-26 04:36:27 +0800

    完成用户templates 添加静态资源

--- jms/settings.py
+++ jms/settings.py
@@ -56,8 +56,7 @@ ROOT_URLCONF = 'jms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),
-                 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
